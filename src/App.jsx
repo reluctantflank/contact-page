5239f7da76778a037b333f5bd1464e37db9d8924
reluctantflank <reluctantflank04@gmail.com>
2024-05-28 23:25:41 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+/** @format */
+
+import "./App.css";
+import Navigation from "./components/Navigation";
+
+function App() {
+	return (
+		<>
+			<Navigation />
+		</>
+	);
+}
+
+export default App;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,20 @@
+/** @format */
+
+import React from "react";
+
+const Navigation = () => {
+	return (
+		<nav>
+			<div>
+				<img src="/images/logo" alt="logo" />
+			</div>
+			<ul>
+				<li>Home</li>
+				<li>About</li>
+				<li>Contact</li>
+			</ul>
+		</nav>
+	);
+};
+
+export default Navigation;
